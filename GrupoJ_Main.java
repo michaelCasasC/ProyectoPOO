24a0207e6335a4ff1c76ab37ababace0412f7d01
michaelCasasC <macasas@espe.edu.ec>
2024-06-02 17:01:42 -0500

    Update GrupoJ_Main.java

--- GrupoJ_Main.java
+++ GrupoJ_Main.java
@@ -1,20 +1,36 @@
 package Proyecto_Grupo_J;
+import java.util.Scanner;
 
 public class GrupoJ_Main {
 
 	public static void main(String[] args) {
+		int opciones;//para el menú
+		Scanner scanner = new Scanner(System.in);
+		GrupoJ_Compra compra= new GrupoJ_Compra();//objeto de la clase GrupoJ_Compra
 		
-		System.out.println("------------------------------------");
-		System.out.println("--                                --");
-		System.out.println("--           BOXPAPER             --");
-		System.out.println("--                                --");
-		System.out.println("------------------------------------");
-		System.out.println(" ");
-		System.out.println("Empresa creadora de productos de papel y carton ");
-		System.out.println("Amigable con la naturaleza");
-		System.out.println("Dueños de la empresa Michael Casas & Lisseth Torres");
-		System.out.println(" ");
+		compra.cabezera();//cabecera de la empreza
 		
+		do { 
+			System.out.println("--------------------MENÚ DE OPCIONES----------------------");
+			System.out.println("1. Ver productos disponibles y precio por unidad");
+			System.out.println("2. Realizar pedido");
+			System.out.println("3. salir");
+			System.out.println("<<<<<<<<<<INGRESE UNA OPCIÓN>>>>>>>>>>>>");
+			System.out.println(" ");
+			opciones=scanner.nextInt();
+			System.out.println(" ");
+			switch(opciones) {
+			case 1:
+				compra.MostrarProductos();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+				break;
+			}
+		}while(opciones!=3);
 		
 	}
 
