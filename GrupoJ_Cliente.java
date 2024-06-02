86ac2aec4f1009eeb9ce2da7b6119e9f92891c3e
LissethGetial06 <155921579+Miasoled@users.noreply.github.com>
2024-06-02 14:55:57 -0500

    Update GrupoJ_Cliente.java

--- GrupoJ_Cliente.java
+++ GrupoJ_Cliente.java
@@ -3,25 +3,41 @@ package Proyecto_Grupo_J;
 import java.util.Scanner;
 
 public class GrupoJ_Cliente {
+	
 	String nomCliente;
 	String numTarjeta;
 	String email;
 	String direccionCliente;
 	String contrasenia;
+	String cedula;
+	String numTelefono;
 	
 	Scanner scanner = new Scanner(System.in);
 	
-	public GrupoJ_Cliente (String nomCliente,String numTarjeta,String email,String direccionCliente,	String contrasenia) {
+	public GrupoJ_Cliente (String nomCliente,String numTarjeta,String email,String direccionCliente,String contrasenia,String cedula, String numTelefono) {
 		this.nomCliente = nomCliente;
 		this.numTarjeta = numTarjeta;
 		this.email = email;
 		this.direccionCliente = direccionCliente;
 		this.contrasenia = contrasenia;
+		this.cedula = cedula;
+		this.numTelefono = numTelefono;
 		
 	}
 	
 	public void IngresarDatosCliente() {
-		System.out.println();
+		System.out.println("------------------------------");
+		System.out.println("Ingrese su nombre y apellido: ");
+		nomCliente = scanner.nextLine();
+		System.out.println("Ingrese su direccion: ");
+		direccionCliente =scanner.nextLine();
+		System.out.println("Ingrese su numero de cedula: ");
+		cedula = scanner.nextLine();
+		System.out.println("Ingrese su correo electronico");
+		email = scanner.nextLine();
+		System.out.println("Ingrese su numero de telefono");
+		numTelefono = scanner.nextLine();
+		System.out.println("------------------------------");
 	}
 	
 	public void RealizarPedido() {
