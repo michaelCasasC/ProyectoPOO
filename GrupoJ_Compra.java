5dde9a78ff3fbc24bc19cb96b730423e89d07cd2
LissethGetial06 <155921579+Miasoled@users.noreply.github.com>
2024-06-02 16:20:08 -0500

    Update GrupoJ_Compra.java

--- GrupoJ_Compra.java
+++ GrupoJ_Compra.java
@@ -1,9 +1,9 @@
 package Proyecto_Grupo_J;
 
 public class GrupoJ_Compra {
-	String []nomProductos;
-	String []nomProcesos;
-	
+	String []nomProductos = {"PAPEL BOND", "PAPEL MINISTRO", "PAPEL CALCO", "PAPEL A3", "PAPEL PERIODICO", "CAJA DE CARTON", "CARTON PRENSADO", "CARTON CORRUGADO", "CARTULINA", "CARTON PIEDRA"};
+	String []nomProcesos;	
+
 	public void MostrarProductos() {
 		
 	}
