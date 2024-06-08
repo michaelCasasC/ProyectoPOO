25d432cfe83794b9c00ed6f9da2479ea573fc75f
michaelCasasC <macasas@espe.edu.ec>
2024-06-07 20:13:15 -0500

    Update GrupoJ_Main.java

--- GrupoJ_Main.java
+++ GrupoJ_Main.java
@@ -7,25 +7,21 @@ public class GrupoJ_Main {
 		int opciones;//para el menú
 		Scanner scanner = new Scanner(System.in);
 		GrupoJ_Compra compra= new GrupoJ_Compra();//objeto de la clase GrupoJ_Compra
-		GrupoJ_Cliente cliente = new GrupoJ_Cliente("","","","","","","",0);//objeto cliente 
-	
+		GrupoJ_Cliente cliente = new GrupoJ_Cliente("","","","","","","");
 		
 		compra.cabezera();//cabecera de la empreza
-		
 		do { 
-			System.out.println("--------------------MENÚ DE OPCIONES----------------------");
-			System.out.println("1. Ver productos disponibles y precio por unidad");
-			System.out.println("2. Realizar pedido");
-			System.out.println("3. salir");
-			System.out.println("<<<<<<<<<<INGRESE UNA OPCIÓN>>>>>>>>>>>>");
-			System.out.println(" ");
+			compra.menuPrincipal();
 			opciones=scanner.nextInt();
 			System.out.println(" ");
+			scanner.nextLine();
 			switch(opciones) {
+			
 			case 1:
 				compra.MostrarProductos();
 				break;
 			case 2:
+				cliente.IngresarDatosCliente();
 				cliente.RealizarPedido();
 				break;
 			case 3:
