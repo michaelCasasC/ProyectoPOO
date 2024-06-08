6c99f3455e341850353d6b721394f2b5e7b131a4
michaelCasasC <macasas@espe.edu.ec>
2024-06-07 20:14:25 -0500

    Update GrupoJ_ProductoPapel.java

--- GrupoJ_ProductoPapel.java
+++ GrupoJ_ProductoPapel.java
@@ -1,12 +1,66 @@
 package Proyecto_Grupo_J;
+import java.util.Scanner;
+
 
 public class GrupoJ_ProductoPapel extends GrupoJ_Productos {
-	 
+	Scanner scanner = new Scanner(System.in);
 	String subProductoPapel;
+	int option;
 	
-	public GrupoJ_ProductoPapel(String subProductoPapel,String nombre, double precio, int tiempoEntrega, int cantProductos) {
-		super(nombre,  precio,  tiempoEntrega,  cantProductos);
+	GrupoJ_Compra compra = new GrupoJ_Compra();
+	
+	public GrupoJ_ProductoPapel(String subProductoPapel,String nombre, double precio, int tiempoEntrega, int cantProductos, double precioVenta) {
+		super(nombre,  precio,  tiempoEntrega,  cantProductos, precioVenta);
 		this.subProductoPapel = subProductoPapel;
 	}
+	
+	public void SeleccionarSubProductoPapel() {
+		do {
+			System.out.println("Productos de papel disponibles: ");
+			System.out.println("1.PAPEL BOND             2.PAPEL MINISTRO "+'\n'
+			                 + "3.PAPEL CALCO            4.PAPEL A3 "+'\n'
+			                 + "5.PAPEL PERIODICO        6.Salir");
+			option=scanner.nextInt();
+			scanner.nextLine();
+			switch(option) {
+			case 1:
+				subProductoPapel=compra.nomProductosPapel[0];
+				precio= compra.preciosPapel[0];
+				break;
+			case 2:
+				subProductoPapel=compra.nomProductosPapel[1];
+				precio= compra.preciosPapel[1];
+				break;
+			case 3:
+				subProductoPapel=compra.nomProductosPapel[2];
+				precio= compra.preciosPapel[2];
+				break;
+			case 4:
+				subProductoPapel=compra.nomProductosPapel[3];
+				precio= compra.preciosPapel[3];
+				break;
+			case 5:
+				subProductoPapel=compra.nomProductosPapel[4];
+				precio= compra.preciosPapel[4];
+				break;
+			case 6:
+				System.out.println("------------saliendo-------------");
+				break;
+			default:
+				System.out.println("opción no disponible");
+				break;
+			}
+			 
+			
+		}while( (option<1 && option>6));
+	}
+		
+	@Override
+	public void MostrarDatos() {
+		System.out.println("Producto: "+nombre);
+		System.out.println("Nombre del Subproducto: "+subProductoPapel);
+		System.out.println("Cantidad: "+cantProductos);
+	}
+	
 
 }
