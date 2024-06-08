0814c5234463c5a6b1ed87eb931d0d772a08ebc4
michaelCasasC <macasas@espe.edu.ec>
2024-06-07 20:15:22 -0500

    Update GrupoJ_Productos.java

--- GrupoJ_Productos.java
+++ GrupoJ_Productos.java
@@ -2,30 +2,37 @@ package Proyecto_Grupo_J;
 import java.util.Scanner;
 
 public class GrupoJ_Productos {
-	Scanner scanner= new Scanner(System.in);
 	String nombre;
 	double precio;
 	int tiempoEntrega;
 	int cantProductos;
+	double precioVenta;
+	Scanner scanner = new Scanner(System.in);
 	
-	public GrupoJ_Productos(String nombre, double precio, int tiempoEntrega, int cantProductos) {
+	public GrupoJ_Productos(String nombre, double precio, int tiempoEntrega, int cantProductos,double precioVenta) {
 		this.nombre = nombre;
 		this.precio = precio;
 		this.tiempoEntrega = tiempoEntrega;
 		this.cantProductos = cantProductos;
+		this.precioVenta=precioVenta;
 	}
 	
 	public void IngresarCantProductos() {
-		System.out.println("ingrese la cantidad de productos que desea ");
-		cantProductos= scanner.nextInt();
+		System.out.println("");
+		cantProductos=scanner.nextInt();
+		
 	}
 	
 	public void PrecioVenta() {
-		System.out.println(" el precio de venta sería "+ (precio*cantProductos));
+		precioVenta=(precio*cantProductos);
+		System.out.println(" el precio de venta sería: "+ precioVenta);
 	}
 	
 	public void MostrarDatos() {
-		
+		System.out.println("Producto: "+nombre);
+		System.out.println("Cantidad: "+cantProductos);
+		System.out.println("precio: "+ precioVenta);
 	}
 
 }
+
