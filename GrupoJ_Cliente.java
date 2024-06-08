d284aab2eefc97592dfa92260b17109eda0891e1
michaelCasasC <macasas@espe.edu.ec>
2024-06-07 20:11:34 -0500

    Update GrupoJ_Cliente.java

--- GrupoJ_Cliente.java
+++ GrupoJ_Cliente.java
@@ -1,125 +1,132 @@
 package Proyecto_Grupo_J;
 
 import java.util.Scanner;
+import java.io.FileWriter;// para escribir archivos
+import java.io.IOException;// para excepciones
 
 public class GrupoJ_Cliente {
-	
-	public String customerName;
-	private String cardNumber;
-	public String email;
-	public String customerAddress;
-	private String password;
-	protected String ci;
-	public String phoneNumber;
-	int option;
-	String cardComp;
-	
+	String nomCliente;
+	String numTarjeta;
+	String email;
+	String direccionCliente;
+	String contrasenia;
+	String cedula;
+	String numTelefono;
+	boolean desicion; //para saber si re realizó la compra o se canceló
 	GrupoJ_Compra compra = new GrupoJ_Compra();
-	GrupoJ_ProductoPapel papel = new GrupoJ_ProductoPapel("","",0,0,0);
-	GrupoJ_ProductoCarton carton = new GrupoJ_ProductoCarton("","","",0,0,0);
 	
 	Scanner scanner = new Scanner(System.in);
+	int option;
+	GrupoJ_ProductoPapel papel = new GrupoJ_ProductoPapel("","papel",0,0,0,0);// objeto de la clase ProductoPapel
+	GrupoJ_ProductoCarton carton = new GrupoJ_ProductoCarton("","carton",0,0,0,0);//objeto de la clase ProductoCarton
 	
-	public GrupoJ_Cliente (String customerName,String cardNumber,String email,String customerAddress,String password,String ci, String phoneNumber, int option) {
-		this.customerName = customerName;
-		this.cardNumber = cardNumber;
+	public GrupoJ_Cliente (String nomCliente,String numTarjeta,String email,String direccionCliente,String contrasenia,String cedula, String numTelefono) {
+		this.nomCliente = nomCliente;
+		this.numTarjeta = numTarjeta;
 		this.email = email;
-		this.customerAddress = customerAddress;
-		this.password = password;
-		this.ci = ci;
-		this.phoneNumber = phoneNumber;
-		this.option = option;
+		this.direccionCliente = direccionCliente;
+		this.contrasenia = contrasenia;
+		this.cedula = cedula;
+		this.numTelefono = numTelefono;
 		
 	}
 	
-	public void IngresarDatosCliente() {
+	public void IngresarDatosCliente() { //registar los datos personales del cliente
 		System.out.println("------------------------------");
 		System.out.println("Ingrese su nombre y apellido: ");
-		customerName = scanner.nextLine();
+		nomCliente = scanner.nextLine();
 		System.out.println("Ingrese su direccion: ");
-		customerAddress =scanner.nextLine();
+		direccionCliente =scanner.nextLine();
 		System.out.println("Ingrese su numero de cedula: ");
-		ci = scanner.nextLine();
+		cedula = scanner.nextLine();
 		System.out.println("Ingrese su correo electronico");
 		email = scanner.nextLine();
 		System.out.println("Ingrese su numero de telefono");
-		phoneNumber = scanner.nextLine();
+		numTelefono = scanner.nextLine();
 		System.out.println("------------------------------");
 	}
 	
-	public void RealizarPedido() {
-		
+	public void RealizarPedido() {// realizar el pedido
 		do {
-		System.out.println("    Seleccionar producto");
-		System.out.println("1.Papel    2.Carton   3.Salir");
-		System.out.println("Ingrese una opcion: ");
-		option = scanner.nextInt();
-		switch (option) {
-		case 1:
-			do { 
-				System.out.println("Producto de papel disponibles");
-				System.out.println("1.PAPEL BOND 2.PAPEL MINISTRO "+'\n'
-						         + "3.PAPEL CALCO 4.PAPEL A3 "+'\n'
-						         + "5.PAPEL PERIODICO 6.Salir");
+			System.out.println("    Seleccionar producto");
+			System.out.println("1.Papel    2.Carton   3.Salir");
+			System.out.println("Ingrese una opcion: ");
+			option = scanner.nextInt();
+			scanner.nextLine();
+			switch (option) {
+			case 1://productos de papel
+				papel.SeleccionarSubProductoPapel();
+				System.out.println("¿cuantas unidades de "+papel.subProductoPapel+" desea?");
+				papel.IngresarCantProductos();
+				
+				papel.PrecioVenta();
+				do {
+				System.out.println("¿Desea realizar el pago?"+'\n'
+						+ "1. sí           2. No"+'\n');
 				option = scanner.nextInt();
 				switch (option) {
 				case 1:
-					papel.precio = compra.preciosPapel[0];
-					papel.subProductoPapel = compra.nomProductosPapel[0];
+					desicion=compra.RealizarPago();
 					break;
-				case 2: 
-					papel.precio = compra.preciosPapel[1];
-					papel.subProductoPapel = compra.nomProductosPapel[1];
-					break;
-				case 3:
-					papel.precio = compra.preciosPapel[2];
-					papel.subProductoPapel = compra.nomProductosPapel[2];
-					break;
-				case 4: 
-					papel.precio = compra.preciosPapel[3];
-					papel.subProductoPapel = compra.nomProductosPapel[3];
-					break;
-				case 5:
-					papel.precio = compra.preciosPapel[4];
-					papel.subProductoPapel = compra.nomProductosPapel[4];
-					break;
-				case 6:
-					System.out.println("Ha salido con exito..");
+				case 2:
+					desicion=compra.CancelarPago();
 					break;
 				default:
-					System.out.println("Opcion incorrecta..");
+					System.out.println(" porfavor,  ingrese 1 o 2");
 					break;
 				}
-				
-			}while(option !=6);
-			papel.IngresarCantProductos();
-			
-			if (compra.RealizarPago()==true) {
-				System.out.println("ingrese el numero de su tarjeta");
-				cardComp= scanner.nextLine();
-				do {
-				if (cardComp==cardNumber) {
-					System.out.println("datos correctos");
-					System.out.println("Nombre = "+customerName);
+				}while(option<1 && option >2);
+				if (desicion==true) {
+					papel.MostrarDatos();
+					System.out.println("gracias por su compra, el pedido llegará en una semana a su dirección específicada");
+					
+					System.out.println("se ha generado una factura tipo csv y una factura tipo json");
 				}else {
-					System.out.println("numero incorrecto, intentelo denuevo");
-		
+					System.out.println("Okey, Gracias por visitar nuestra página..... que tenga un excelente día");
+					return;
 				}
-				}while(cardComp!=cardNumber);
-				
+				break;
+			case 2: // productos de carton 
+				carton.SeleccionarSubProductoCarton();
+				System.out.println("¿cuantas unidades de "+carton.subProductoCarton+" desea?");
+				carton.IngresarCantProductos();
+			
+				carton.PrecioVenta();
+				do {
+				System.out.println("¿Desea realizar el pago?"+'\n'
+						+ "1. sí           2. No"+'\n');
+				option = scanner.nextInt();
+				switch (option) {// para saber se se cancela o se lleva a cabo el pago 
+				case 1:
+					desicion=compra.RealizarPago();
+					break;
+				case 2:
+					desicion=compra.CancelarPago();
+					break;
+				default:
+					System.out.println(" porfavor,  ingrese 1 o 2");
+					break;
+				}
+				}while(option<1 && option >2);
+				if (desicion==true) {
+					carton.MostrarDatos();
+					System.out.println("gracias por su compra, el pedido llegará en una semana a su dirección específicada");
+					
+					System.out.println("se ha generado una factura tipo csv y una factura tipo json");
+				}else {
+					System.out.println("Okey, Gracias por visitar nuestra página..... que tenga un excelente día");
+					return;
+				}
+				break;
+			case 3:
+				System.out.println("saliendo.......");
+				break;
+			default:
+				System.out.println("ingrese una opción válida");
+				break;
 			}
-			
-			
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		default: 
-			break;
-		}
-		}while(option !=3);
 
-	}
-	
+	}while(option !=3);
+
+}
 }
