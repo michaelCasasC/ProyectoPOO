7508dc5063d2882080bbdbf077ce7572a3c29c80
michaelCasasC <macasas@espe.edu.ec>
2024-06-08 22:18:40 -0500

    Update GrupoJ_Compra.java

--- GrupoJ_Compra.java
+++ GrupoJ_Compra.java
@@ -2,8 +2,10 @@ package Proyecto_Grupo_J;
  import java.util.Scanner;
 public class GrupoJ_Compra {
 	
+	    //arreglos con los nombres de los subproductos de papel y precios
 		String []nomProductosPapel = {"PAPEL BOND", "PAPEL MINISTRO", "PAPEL CALCO", "PAPEL A3", "PAPEL PERIODICO"};
 		double [] preciosPapel= {0.10 , 0.30, 0.15, 0.40, 0.25 };
+		//arreglos con los nombres de los subproductos de carton y precios
 		String []nomProductosCarton = {"CAJA DE CARTON", "CARTON PRENSADO", "CARTON CORRUGADO", "CARTULINA", "CARTON PIEDRA"};
 		double [] preciosCarton= {0.90 , 0.75, 0.67, 0.80, 1.20 };
 		Scanner scanner= new Scanner(System.in);
@@ -25,7 +27,7 @@ public class GrupoJ_Compra {
 		System.out.println("--------------------MENÚ DE OPCIONES----------------------");
 		System.out.println("1. Ver productos disponibles y precio por unidad");
 		System.out.println("2. Realizar pedido");
-		System.out.println("3. salir");
+		System.out.println("3. Salir");
 		System.out.println("<<<<<<<<<<INGRESE UNA OPCIÓN>>>>>>>>>>>>");
 		System.out.println(" ");
 	}
@@ -33,9 +35,9 @@ public class GrupoJ_Compra {
 	  int opciones;
 	  do {
 		  System.out.println(" -------ESCOJA EL TIPO DE PRODUCTO-----------");
-		  System.out.println("1. ver productos de papel");
-		  System.out.println("2. ver productos de carton");
-		  System.out.println("3. regresar");
+		  System.out.println("1. Ver productos de papel");
+		  System.out.println("2. Ver productos de carton");
+		  System.out.println("3. Regresar");
 		  System.out.println(" ------------INGRESE UNA OPCIÓN---------------");
 		  opciones=scanner.nextInt();
 		  scanner.nextLine();
@@ -70,11 +72,11 @@ public class GrupoJ_Compra {
 		
 	}
 	
-	public boolean RealizarPago() {
+	public boolean RealizarPago() {// decvuelve un valor verdadero para usarlo en la clase cliente
 		return true;
 	}
 	
-	public boolean CancelarPago() {
+	public boolean CancelarPago() {// devuelve un valor falso para usarlo en la clase cliente
 		return false;
 	}
 
